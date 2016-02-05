546227e9a0c61bc4ac111ac560c9d3bc19ad4505
srtianxia <srtianxia@163.com>
2016-02-06 01:16:55 +0800

    add

---
+++ setEssay.php
@@ -0,0 +1,25 @@
+<?php
+include("connect.php");
+include("token.php");
+
+$authorId = checkToken(addslashes($_POST["token"]),$returnData);
+if ($authorId == -1) {
+  echo json_encode($returnData);
+  return;
+}
+
+$title = addslashes($_POST["title"]);
+$content = addslashes($_POST["content"]);
+$isPrivate = addslashes($_POST["isPrivate"]);
+$sql = "INSERT INTO (authorId,title,content,isPrivate)
+VALUES ('".$authorId."','".$title."','".$content."',now())";
+
+if (mysql_query($sql)) {
+		$returnData["info"] = $sql;
+	}else{
+		header("http/1.1 500 Internal Server Error");
+		$returnData["error"] = mysql_error();
+	}
+
+ 	echo json_encode($returnData);
+ ?>
